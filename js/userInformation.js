e3b8ae3cbdfbfd9964ffaa7bd6e415cdf7897e59
ZakLems <85376589+ZakLems@users.noreply.github.com>
2022-04-01 10:49:52 -0400

    basic objects

---
+++ js/userInformation.js
@@ -0,0 +1,18 @@
+function userBasics(){
+    this.name;
+    this.gradeLevel;
+    this.ID;
+    this.coursesTaken;
+    this.totalCredits
+}
+
+function course() {
+    this.name;
+    this.graduationRequirement;
+    this.prerequisites;
+    this.creditValue;
+    this.gradeWeight;
+    this.term;
+    this.department;
+    this.finalGrade;
+}
