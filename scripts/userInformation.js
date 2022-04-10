766f6d7daa44e355c4e484d5da496408f12a412d
ZakLems <85376589+ZakLems@users.noreply.github.com>
2022-04-10 02:28:37 -0400

    graduation req

--- scripts/userInformation.js
+++ scripts/userInformation.js
@@ -1,19 +1,29 @@
-function userBasics(name, gradeLevel, studentID, coursesTaken, interests, totalCredits){
-    this.name = name;
-    this.gradeLevel = gradeLevel;
-    this.studentID = studentID;
-    this.coursesTaken = coursesTaken;
-    this.interests = interests;
-    this.totalCredits = totalCredits;
-}
+//Definitions
+var graduationRequirements = [
+	"Fine Art",
+	"English 11",
+	"English 12",
+	"English 10",
+	"English 9",
+	"Financial Literacy",
+	"Health",
+	"Physical Education",
+	"Math",
+	"Science",
+	"Biology",
+	"Government",
+	"World History",
+	"U.S. History",
+	"Technology Education",
+	"Language",
+];
 
-function course() {
-    this.name;
-    this.graduationRequirement;
-    this.prerequisites;
-    this.creditValue;
-    this.gradeWeight;
-    this.term;
-    this.department;
-    this.finalGrade;
+//basic user information
+function userBasics(name, gradeLevel, studentID, coursesTaken) {
+	this.name = name;
+	this.gradeLevel = gradeLevel;
+	this.studentID = studentID;
+	this.coursesTaken = coursesTaken;
 }
+var userInfo = new userBasics("Razak Diallo", 9, 100342969, ["A1", "B"]);
+console.log(userInfo);
