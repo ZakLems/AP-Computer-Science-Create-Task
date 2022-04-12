05541138b2e174f26d2c8a396577c3e9c943de74
ZakLems <85376589+ZakLems@users.noreply.github.com>
2022-04-12 02:11:59 -0400

    Find prerequisites

--- scripts/userInformation.js
+++ scripts/userInformation.js
@@ -1,18 +1,15 @@
 const fs = require("fs");
 
 //Definitions
-fs.readFile("./scripts/data/courseData.json", "utf-8", (err, jsonString) => {
-	if (err) {
-		console.log(err);
-	} else {
-		try {
-			const data = JSON.parse(jsonString);
-			//console.log(data);
-		} catch (err) {
-			console.log("Error parsing JSON", err);
-		}
-	}
-});
+let data;
+try {
+	data = fs.readFileSync("./scripts/data/courseData.json", "utf-8");
+	data = JSON.parse(data);
+	//console.log(data);
+} catch (err) {
+	console.log(err);
+}
+
 var graduationRequirements = [
 	"Fine Art",
 	"English 11",
@@ -40,4 +37,16 @@ function userBasics(name, gradeLevel, studentID, coursesTaken) {
 	this.coursesTaken = coursesTaken;
 }
 var userInfo = new userBasics("Razak Diallo", 9, 100342969, ["A1", "B"]);
-//console.log(userInfo);
+
+//find prerequisites
+const foundArray = [];
+for (let i = 0; i < userInfo.coursesTaken.length; i++) {
+	console.log(userInfo.coursesTaken[i]);
+	found = data.findIndex(function (course, index) {
+		if (course.id == userInfo.coursesTaken[i]) {
+			return true;
+		}
+	});
+	foundArray.push(data[found]);
+}
+console.log(foundArray);
