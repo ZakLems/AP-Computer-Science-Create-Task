458b10b165380f1fe8f60fc4b1db93f5732c13e9
ZakLems <85376589+ZakLems@users.noreply.github.com>
2022-04-25 04:36:26 -0400

    working deletion list and completion of addcourses

--- scripts/userInformation.js
+++ scripts/userInformation.js
@@ -53,7 +53,7 @@ function selectableCoursesBasedOnPrerequisites(courses, year) {
 		var allCourses = courses["preHighschool"].concat(courses["freshman"], courses["sophomore"], courses["junior"]);
 	}
 	var allowedCourses = [];
-	if (year == 0) {
+	if (year == 0 || allCourses.length == 0) {
 		allowedCourses = data.filter((course) => course.prerequisites == "");
 	} else {
 		for (let i = 0; i < allCourses.length; i++) {
@@ -62,11 +62,10 @@ function selectableCoursesBasedOnPrerequisites(courses, year) {
 			);
 		}
 	}
-
 	return allowedCourses.filter(onlyUnique);
 }
 
-function selectablecoursesBasedOnGradeLevel(year) {
+function selectableCoursesBasedOnGradeLevel(year) {
 	var allowedCourses = [];
 	if (year == 1) {
 		allowedCourses = data.filter((course) => course.gradeLevel.includes(9));
@@ -84,6 +83,76 @@ function selectablecoursesBasedOnGradeLevel(year) {
 	return allowedCourses.filter(onlyUnique);
 }
 
+var currentSelectedYear = 0;
+let userCourses = { preHighschool: [], freshman: [], sophomore: [], junior: [], senior: [] };
+
+function addCourses(coursesToAdd) {
+	//determine current selected year name
+	let currentSelectedYearName;
+	if (currentSelectedYear == 0) {
+		currentSelectedYearName = "preHighschool";
+	} else if (currentSelectedYear == 1) {
+		currentSelectedYearName = "freshman";
+	} else if (currentSelectedYear == 2) {
+		currentSelectedYearName = "sophomore";
+	} else if (currentSelectedYear == 3) {
+		currentSelectedYearName = "junior";
+	} else if (currentSelectedYear == 4) {
+		currentSelectedYearName = "senior";
+	}
+	//add selected courses as object as referenced from data variable to master course list
+	userCourses[currentSelectedYearName] = courseArraytoCourseArrayOBJ(coursesToAdd);
+
+	//determine if user can take selected courses based on prerequisites and grades allowed to take course. Remove if lacking required prerequisites or not offered in grade (with exceptions for courses that require prerequisites to allow pre-highschool advancement).
+	let deletionList = [];
+
+	for (let i = 0; i < userCourses[currentSelectedYearName].length; i++) {
+		if (
+			selectableCoursesBasedOnPrerequisites(userCourses, currentSelectedYear)
+				.map((courses) => courses.id)
+				.includes(userCourses[currentSelectedYearName][i].id)
+		) {
+			console.log(`${userCourses[currentSelectedYearName][i].name} has the required prerequisites`);
+		} else {
+			console.log(
+				`${userCourses[currentSelectedYearName][i].name} doesn't have the required prerequisites! Removing from list.`
+			);
+			deletionList.push(userCourses[currentSelectedYearName][i].id);
+		}
+	}
+	console.log("");
+	if (currentSelectedYearName != "preHighschool") {
+		for (let i = 0; i < userCourses[currentSelectedYearName].length; i++) {
+			if (userCourses[currentSelectedYearName][i].prerequisites == "") {
+				if (
+					selectableCoursesBasedOnGradeLevel(currentSelectedYear)
+						.map((courses) => courses.id)
+						.includes(userCourses[currentSelectedYearName][i].id)
+				) {
+					console.log(
+						`${userCourses[currentSelectedYearName][i].name} is in the appropriate grade level for the student.`
+					);
+				} else {
+					console.log(
+						`${userCourses[currentSelectedYearName][i].name} doesn't provide classes for the student's grade level! Removing from list.`
+					);
+					deletionList.push(userCourses[currentSelectedYearName][i].id);
+				}
+			}
+		}
+	}
+	console.log(deletionList);
+	userCourses[currentSelectedYearName] = userCourses[currentSelectedYearName].filter(function (e) {
+		return !deletionList.includes(e.id);
+	});
+}
+currentSelectedYear = 1;
+for (let i = 0; i < 1; i++) {
+	addCourses(["A1", "B", "E9", "MP1", "CS", "G"]);
+	currentSelectedYear += 1;
+}
+
+//graduation requirements
 const graduationRequirements = [
 	"Fine Art",
 	"English 11",
@@ -102,75 +171,3 @@ const graduationRequirements = [
 	"Technology Education",
 	"Language",
 ];
-
-var currentSelectedYear = 0;
-let userCourses = { preHighschool: ["piss"], freshman: [], sophomore: [], junior: [], senior: [] };
-
-function addCourses(coursesToAdd) {
-	//determine current selected year name
-	let currentSelectedYearName;
-	if (currentSelectedYear == 0) {
-		currentSelectedYearName = "preHighschool";
-	} else if (currentSelectedYear == 1) {
-		currentSelectedYearName = "freshman";
-	} else if (currentSelectedYear == 2) {
-		currentSelectedYearName = "sophomore";
-	} else if (currentSelectedYear == 3) {
-		currentSelectedYearName = "junior";
-	} else if (currentSelectedYear == 4) {
-		currentSelectedYearName = "senior";
-	}
-	//add selected courses as object as refrenced from data variable to master course list
-	userCourses[currentSelectedYearName] = courseArraytoCourseArrayOBJ(coursesToAdd);
-
-	//determine if user can take selected courses based on prerequisties and grades allowed to take course. Remove if lacking required prerequisites or not in grade.
-	let deletionList = [];
-	for (let i = 0; i < userCourses[currentSelectedYearName].length; i++) {
-		if (
-			selectableCoursesBasedOnPrerequisites(userCourses, currentSelectedYear)
-				.map((courses) => courses.id)
-				.includes(userCourses[currentSelectedYearName][i].id)
-		) {
-			console.log(`${userCourses[currentSelectedYearName][i].name} has the required prerequisites`);
-		} else {
-			console.log(
-				`${userCourses[currentSelectedYearName][i].name} doesn't have the required prerequisites! Removing from list.`
-			);
-			deletionList.push(userCourses[currentSelectedYearName][i].name);
-		}
-	}
-
-	if (currentSelectedYearName != "preHighschool") {
-		for (let i = 0; i < userCourses[currentSelectedYearName].length; i++) {
-			if (
-				selectablecoursesBasedOnGradeLevel(currentSelectedYear)
-					.map((courses) => courses.id)
-					.includes(userCourses[currentSelectedYearName][i].id)
-			) {
-				console.log(
-					`${userCourses[currentSelectedYearName][i].name} is in the appropriate grade level for the student.`
-				);
-			} else {
-				console.log(
-					`${userCourses[currentSelectedYearName][i].name} doesn't provide classes for the student's grade level! Removing from list.`
-				);
-				deletionList.push(userCourses[currentSelectedYearName][i].name);
-			}
-		}
-	}
-	deletionList = deletionList.filter(function (e) {
-		return e != "Algebra 1";
-	});
-	for (let i = 0; i < deletionList.length; i++) {
-		userCourses[currentSelectedYearName] = userCourses[currentSelectedYearName].filter(function (e) {
-			return e.name != deletionList[i];
-		});
-	}
-}
-currentSelectedYear = 1;
-for (let i = 0; i < 1; i++) {
-	addCourses(["G", "A1", "B", "E9", "MP1"]);
-	currentSelectedYear += 1;
-}
-
-console.log(userCourses);
